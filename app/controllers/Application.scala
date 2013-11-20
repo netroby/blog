fe39fb0a893b9206d37c927499412f11915397da
zhifeng hu <zf@ancientrocklab.com>
2013-11-21 06:32:05 +0800

    Connect to a database

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,21 @@
 package controllers
-
-import play.api._
+import play.api.Play.current
 import play.api.mvc._
+import play.api.db._
+import views._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok("Hello World")
+    var outString = ""
+    val conn = DB.getConnection()
+    val stmt = conn.createStatement
+    val rs = stmt.executeQuery("SELECT * FROM articles ")
+    while (rs.next()) {
+       outString += rs.getString("title")+ "\n" + rs.getString("content")
+    }
+    conn.close()
+    Ok(html.index(outString))
   }
 
 }
