8c3e79bdf8374ef7ccaf21dd40b04e96cb6dcac5
zhifeng hu <hzfeng@vip.deyi.com>
2013-11-21 10:15:31 +0800

    Test JDBC exmaple

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,20 +2,19 @@ package controllers
 import play.api.Play.current
 import play.api.mvc._
 import play.api.db._
-import views._
 
 object Application extends Controller {
 
-  def index = Action {
-    var outString = ""
-    val conn = DB.getConnection()
-    val stmt = conn.createStatement
-    val rs = stmt.executeQuery("SELECT * FROM articles ")
-    while (rs.next()) {
-       outString += rs.getString("title")+ "\n" + rs.getString("content")
-    }
-    conn.close()
-    Ok(html.index(outString))
-  }
+	def index = Action {
+		var outString = "Number is "
+			val conn = DB.getConnection()
+			val stmt = conn.createStatement
+			val rs = stmt.executeQuery("SELECT 9 as testkey ")
+			while (rs.next()) {
+				outString += rs.getString("testkey")
+			}
+		conn.close()
+		Ok(outString)
+	}
 
 }
